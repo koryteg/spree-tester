856b9efff540162c714657ba24b588f2565f4dff
koryteg <korytegman@gmail.com>
2013-06-27 13:48:35 -0700

    adding to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,11 @@
 require "bundler/capistrano"
 
-server "ec2-54-218-231-87.us-west-2.compute.amazonaws.com", :web, :app, :db, primary: true
+server "ec2-54-214-206-253.us-west-2.compute.amazonaws.com", :web, :app, :db, primary: true
 set :application, "spree-tester"
-set :user, "bitnami"
-set :deploy_to, "/opt/#{user}/projects/#{application}"
-set :deploy_via, :remote_cache
-set :use_sudo, true
+set :user, "deployer"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :copy
+set :use_sudo, false
 
 set :scm, "git"
 set :repository, "git@github.com:koryteg/spree-tester.git"
@@ -17,6 +17,38 @@ ssh_options[:keys] = [File.join(ENV["HOME"], ".ec2", "meetearl")]
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
+namespace :deploy do
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "/etc/init.d/unicorn_#{application} #{command}"
+    end
+  end
+
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    run "mkdir -p #{shared_path}/config"
+    # put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end
+  after "deploy:setup", "deploy:setup_config"
+
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
+end
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
 
